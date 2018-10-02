c49f70690b202ab7fc22027ab22a989dbff8e109
Asael <asael2@gmail.com>
2018-10-02 15:51:23 -0500

    Commit Check

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 import { UsuariosProvider } from '../../providers/usuarios/usuarios';
 
 @Component({
@@ -10,20 +11,33 @@ import { UsuariosProvider } from '../../providers/usuarios/usuarios';
 export class ContactPage {
 
   usuarios:any;
+  // loading :any;
+  
 
-  constructor(public navCtrl: NavController, public UsuariosProvider: UsuariosProvider) {
+  constructor(public navCtrl: NavController, public UsuariosProvider: UsuariosProvider, public loader: LoadingController ) {
+    this.presentLoading()
+  }
+
+  presentLoading() {
+    const loader = this.loader.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
   }
 
   ionViewDidLoad(){
+
     this.UsuariosProvider.getUsuarios().subscribe(
       (data) => {
         this.usuarios = data;
         console.log(this.usuarios);
+        this.loader.config
       },
       (error) =>{
         console.error(error);
       }
     )
   }
-  
+
 }
