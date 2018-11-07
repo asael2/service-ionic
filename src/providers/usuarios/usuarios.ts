4e03c1f7e7fdbf7e3310f96601937a4dd498a447
Asael <asael2@gmail.com>
2018-11-06 23:57:17 -0500

    getting token

--- src/providers/usuarios/usuarios.ts
+++ src/providers/usuarios/usuarios.ts
@@ -1,15 +1,32 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UsuariosProvider {
 
+  
   constructor(public http: HttpClient) {
-   //this.getUsuarios();
+    //this.getUsuarios();
+
+    this.loguearse().subscribe(val => console.log(val));
+
   }
 
   getUsuarios(){
     return this.http.get('https://miapirails.herokuapp.com/users');
+  }
+
+
+  loguearse() {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    let bodies = { "auth": { "email": "salma@stars.com", "password": "qwertyuiop" } }
+    let options = { headers: headers };
+    
+    
+    return this.http.post('https://miapirails.herokuapp.com/user_token', bodies, options);
+
   }
   
 }
